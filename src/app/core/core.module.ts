35e5f1469a6c6cf9ec3e39790843f059a2fab08e
Vincent Jeannin <jeannin.vincent@gmail.com>
2024-05-29 15:22:17 +0200

    feat(#2) : Add core service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SafeHtmlToPlainTextPipe } from './pipes/safe-html-to-plain-text.pipe';
+
 @NgModule({
-  declarations: [],
+  declarations: [SafeHtmlToPlainTextPipe],
   imports: [CommonModule],
+  exports: [SafeHtmlToPlainTextPipe],
 })
 export class CoreModule {}
---
+++ src/app/core/services/core.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { SquadBustersFRApi } from '@vinz-sc/squadbustersfr-api';
+
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoreService {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+  |*                          CONSTANTS                          *|
+  \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+  private readonly SESSION_KEY = 'squadbustersfr-theme';
+
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+  |*                          PROPERTIES                         *|
+  \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+  private readonly _api: SquadBustersFRApi;
+  private _preferredTheme?: 'dark' | 'light';
+
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+  |*                        CONSTRUCTORS                         *|
+  \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+  constructor() {
+    this._api = new SquadBustersFRApi(environment.apiUrl);
+  }
+
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+  |*                           PUBLIC                            *|
+  \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+  /* * * * * * * * * * * * * * * *\
+  |*           GETTERS           *|
+  \* * * * * * * * * * * * * * * */
+
+  public get api(): SquadBustersFRApi {
+    return this._api;
+  }
+}
